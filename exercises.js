b0399599b53cf595d4c7ab9657b5ae5fcaa05d34
phytertek <ryan.phytertek@gmail.com>
2017-07-26 02:57:27 -0400

    problem 8 passed

--- exercises.js
+++ exercises.js
@@ -61,22 +61,24 @@ function isPrime(num) {
     //hint: a prime number is only evenly divisible by itself and 1
     //hint2: you can solve this using a for loop
     //note: 0 and 1 are NOT considered prime numbers
-    if (isNaN(num) || !isFinite(num) || num % 1 || num < 2) return false
-    if (num % 2 === 0) return num === 2
-    if (num % 3 === 0) return num === 3
-    var m = Math.sqrt(num)
+    if (isNaN(num) || !isFinite(num) || num % 1 || num < 2) return false;
+    if (num % 2 === 0) return num === 2;
+    if (num % 3 === 0) return num === 3;
+    var m = Math.sqrt(num);
     for (let i = 5; i <= m; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) return false
+        if (num % i === 0 || num % (i + 2) === 0) return false;
     }
-    return true
+    return true;
 }
 
 function returnFirst(arr) {
     //return the first item from the array
+    return arr[0];
 }
 
 function returnLast(arr) {
     //return the last item of the array
+    return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
