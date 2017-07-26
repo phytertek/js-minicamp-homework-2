8773d9c30209ea8088a0c28c96e2566d5a5719d9
phytertek <ryan.phytertek@gmail.com>
2017-07-26 03:09:58 -0400

    problem 16 passed

--- exercises.js
+++ exercises.js
@@ -131,6 +131,7 @@ function addNumbers(numbers) {
 function averageTestScore(testScores) {
     //testScores is an array.  Iterate over testScores and compute the average.
     //return the average
+    return testScores.reduce((acc, cv) => acc + cv) / testScores.length
 }
 
 function largestNumber(numbers) {
